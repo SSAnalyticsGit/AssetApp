4162a519d1c006af128a6e31af882a7dd623cd87
SSAnalyticsGit <86230494+SSAnalyticsGit@users.noreply.github.com>
2023-02-03 10:59:58 +0100

    Update settings.py

--- AssetApp/settings.py
+++ AssetApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ['ASSETAPP_KEY']
+SECRET_KEY = ['ASSET_APP_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
