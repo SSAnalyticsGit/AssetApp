7a31c3ea9f762d26d6ba4462dd1c8b07cc1a7abd
SSAnalyticsGit <itsupport@starsightenergy.com>
2023-02-27 17:48:22 +0100

    fix

--- AssetRequests/urls.py
+++ AssetRequests/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('user/profile/', views.UserProfile, name='UserProfile'),
-    path('accounts/login/', auth_views.LoginView.as_view(next_page=''), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(next_page='index'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('<int:id>/', views.MakeRequestView, name='makerequest'),
     path('user/', views.ManagerAllocation, name='ManagerSchedule'),
